67488ff199f73cf5ed8d66b78c8e3083adc1421d
hyuan10 <h.yuan10@newcastle.ac.uk>
2021-12-20 23:17:55 +0000

    First commit

---
+++ src/Setup-Script.R
@@ -0,0 +1,11 @@
+# Script to set up Project
+setwd("~/Documents/CSC_8634/")
+
+library('ProjectTemplate')
+load.project()
+
+# for (dataset in project.info$data)
+# {
+#   message(paste('Showing top 5 rows of', dataset))
+#   print(head(get(dataset)))
+# }
