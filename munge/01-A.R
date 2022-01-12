56d428848e29af39e6d2c9bbec50e31d61276001
hyuan10 <h.yuan10@newcastle.ac.uk>
2022-01-12 16:15:51 +0000

    Data wrangling preprocess script

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,31 @@
-# Example preprocessing script.
+#This preprocess script will combine the data sub sets together
+
+library(dplyr)
+library(readr)
+
+# Setting up the data files as data frames
+
+df.database.1<-as.data.frame(fread("14615503_1224523740902654_6852172664916672512_n"))
+
+df.database.2<-as.data.frame(fread("10000000_220627975019916_5597249363521830912_n"))
+
+df.database.3<-as.data.frame(fread("10000000_775780395897194_5651293731576348672_n"))
+
+df.database.4<-as.data.frame(fread("10000000_1038929352871690_5384801718213017600_n"))
+
+
+# Combines all the data frames into 1
+
+df.database.binded<-rbind(df.database.1,df.database.2,df.database.3,df.database.4)
+
+# Rename Column Names
+
+colnames(df.database.binded) <- c("Timestamp","packet length (1)", "anonymized(2) source (src) IP","anonymized(2) destination (dst) IP","anonymized source (src) L4 Port",
+                                 "anonymized destination (dst) L4 Port","IP protocol","anonymized source (src) hostprefix (3)","anonymized destination (dst) hostprefix (3)",
+                                 "anonymized source (src) Rack", "anonymized destination (dst) Rack", "anonymized source (src) Pod", "anonymized destination (dst) Pod",
+                                 "intercluster", "interdatacenter")
+
+# Reformats Timestamp to Human readable
+
+date.time = data.frame("Datetime Timestamp" = c(as.POSIXct(df.database.binded$Timestamp, origin="1970-01-01", tz="GMT")))
+
