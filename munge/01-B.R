bde48f8e6b7f1131785709e222612bc4d1de7eae
hyuan10 <h.yuan10@newcastle.ac.uk>
2022-01-16 19:19:40 +0000

    Renamed dataframes to correct month

--- munge/01-B.R
+++ munge/01-B.R
@@ -8,23 +8,30 @@ print(ppd()*-1)
 
 # Filters the entire Data frame into two lots of data frames sorted by day
 
-df.jan01=filter(df.database,Day=="01")  # Jan 01 filtered data
+df.oct01=filter(df.database,Day=="01")  # Oct 01 filtered data
 
-df.jan02=filter(df.database,Day=="02")  # Jan 02 filtered data
+df.oct02=filter(df.database,Day=="02")  # Oct 02 filtered data
 
 # Sorts the filtered data by Hour, Mins and Secs
 
-attach(df.jan01)
-df.Sorted1<-df.jan01[order(Hour,Mins,Secs),]
-detach(df.jan01)
+attach(df.oct01)
+df.Sorted1<-df.oct01[order(Hour,Mins,Secs),]
+detach(df.oct01)
 
 
-attach(df.jan02)
-df.Sorted2<-df.jan02[order(Hour,Mins,Secs),]
-detach(df.jan02)
+attach(df.oct02)
+df.Sorted2<-df.oct02[order(Hour,Mins,Secs),]
+detach(df.oct02)
 
 # Creates data frames for the grouped and summed data
 
-df.gjan01 <- df.Sorted1 %>% group_by(Time) %>% summarise(sum_packet = sum(`packet length (1)`))
+df.goct01 <- df.Sorted1 %>% 
+  group_by(Time) %>% 
+  summarise(sum_packet = sum(`packet length (1)`))
 
-df.gjan02 <- df.Sorted2 %>% group_by(Time) %>% summarise(sum_packet = sum(`packet length (1)`))
+df.goct02 <- df.Sorted2 %>% 
+  group_by(Time) %>% 
+  summarise(sum_packet = sum(`packet length (1)`))
+
+ 
+df.goct02$Date <- "Oct 02"
