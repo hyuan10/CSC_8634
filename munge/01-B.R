5ee790772e96acf8d57ba049f10fc2928755d60a
hyuan10 <h.yuan10@newcastle.ac.uk>
2022-01-19 02:26:44 +0000

    Complete Data frame for plotting

--- munge/01-B.R
+++ munge/01-B.R
@@ -1,3 +1,5 @@
+# Packet Sum
+
 # Counts individual packets for each day
 
 ppd = function(){
@@ -23,23 +25,38 @@ attach(df.oct02)
 df.Sorted2<-df.oct02[order(Hour,Mins,Secs),]
 detach(df.oct02)
 
+
 # Creates data frames for the grouped and summed data
 
-df.goct01 <- df.Sorted1 %>% 
-  group_by(Time) %>% 
-  summarise('Sum Packet Length' = sum(`packet length (1)`))
+df.goct01 <- df.Sorted1 %>% group_by(Date, Hour) %>%summarise(Sum.Packets=sum(`packet length (1)`))
 
-df.goct02 <- df.Sorted2 %>% 
-  group_by(Time) %>% 
-  summarise('Sum Packet Length' = sum(`packet length (1)`))
+ungroup(df.goct01)
 
-# Adding in a date column back into the dataframe
+df.goct02 <- df.Sorted2 %>% group_by(Date, Hour) %>%summarise(Sum.Packets=sum(`packet length (1)`))
 
-df.goct01 <- df.goct01 %>% add_column(Date= "01 Oct",.before="Time")
+ungroup(df.goct02)
 
-df.goct02 <- df.goct02 %>% add_column(Date= "02 Oct",.before="Time")
+
+
+# Adding a concatinated column of date and time to the Hour of the packets
+
+df.goct01$Time <- paste(df.goct01$Date, df.goct01$Hour)
+
+df.goct02$Time <- paste(df.goct02$Date, df.goct02$Hour)
+
+# Reformats the Time column to POSIXct format
+
+df.goct01$Time <- as.POSIXct(df.goct01$Time, origin="1970-01-01", tz="GMT")
+
+df.goct02$Time <- as.POSIXct(df.goct02$Time, origin="1970-01-01", tz="GMT")
+
 
 # Combining the dataframes to be plot into one
 
 df.gb.database<- rbind(df.goct01, df.goct02)
+
+# Reordering columns
+
+df.gb.database<-subset(df.gb.database, select=c(1,2,4,3))
+
 
