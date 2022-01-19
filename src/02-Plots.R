5171f617047e5c58227589254b048378817edcc2
hyuan10 <h.yuan10@newcastle.ac.uk>
2022-01-19 02:35:36 +0000

    Correction to Label

--- src/02-Plots.R
+++ src/02-Plots.R
@@ -2,9 +2,11 @@
 
 library(scales)
 
+library(ggplot2)
+
 p1 <- ggplot(data=df.gb.database, aes(x=Time, y=Sum.Packets)) +
-  ggtitle("Packets Received Over Time")+
-  labs(x="Time", y= "Packets Received",caption="Graph 1")+
+  ggtitle("Sum of Packet Length Over Time")+
+  labs(x="Time", y= "Sum of Packet Lenth",caption="Graph 1")+
   geom_bar(stat="identity", fill="#08588D")+
   scale_x_datetime(labels = date_format("%H:%M", tz="GMT"))+
   scale_y_continuous(labels = comma)+
